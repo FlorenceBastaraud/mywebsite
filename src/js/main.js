5fd9986d599290cfd160426712559338cec33342
Florence Bastaraud <fbastaraud@yahoo.fr>
2024-09-24 12:32:16 +0200

    hero section

--- src/js/main.js
+++ src/js/main.js
@@ -1,105 +1,244 @@
-document.addEventListener('DOMContentLoaded', (e) => {
+// animations
+window.addEventListener('load', () => {
+  const tl = gsap.timeline()
+  const ball = '.preload-ball'
+  const playground = '.preload-playground'
+  const workTitle = '.content__work-title'
+  const nav = '.header__content--nav'
+  const socials = '.footer__content--links'
+  const copyr = '.footer__content--copyrights'
+  const logo = '.header__content--logo'
+  const heroIllust = '#hero-illust'
+  const particles = '#particles-js'
 
+  tl.to(ball, {
+    y: '80vh',
+    scale: 1,
+    duration: 2,
+    ease: 'bounce.out',
+  })
 
-  // horizontal scrolling
-  const mainWrapper = document.querySelector('main');
-  const projectsWrapper = document.getElementById('projects');
+  tl.to(
+    ball,
+    {
+      x: '110vw',
+      duration: 3,
+      ease: 'power1.inOut',
+    },
+    'preload'
+  )
 
-  mainWrapper.addEventListener('wheel', (e) => {
-        
-    if(screen.width > 840){
-      
-      if(e.deltaY > 0){
-              
-        if(projectsWrapper.getBoundingClientRect().x == 0){
-                    
-          mainWrapper.classList.add('vertical-scroll');
-          document.body.classList.add('vertical-scroll');
-          document.querySelector('footer').classList.add('vertical-scroll');
-          
-        } else {
-          
-          e.preventDefault();
-          mainWrapper.classList.remove('vertical-scroll');
-          document.body.classList.remove('vertical-scroll');
-          document.querySelector('footer').classList.remove('vertical-scroll');
+  tl.to(
+    ball,
+    {
+      backgroundColor: '#070707',
+      duration: 1.5,
+      ease: 'power1.inOut',
+    },
+    'preload'
+  )
 
-          mainWrapper.scrollLeft += screen.width;
+  tl.to(
+    playground,
+    {
+      x: '-110vw',
+      duration: 3,
+      ease: 'power1.inOut',
+    },
+    'preload'
+  )
 
-        }
-        
-      } else {
+  tl.to(
+    heroIllust,
+    {
+      duration: 4,
+      rotationY: 360,
+      ease: 'power2.out',
+    },
+    'preload'
+  )
 
-        
-        if(projectsWrapper.getBoundingClientRect().top > 0){
-          mainWrapper.classList.remove('vertical-scroll');
-          document.body.classList.remove('vertical-scroll');
-          document.querySelector('footer').classList.remove('vertical-scroll');
+  tl.to(
+    workTitle,
+    {
+      left: 0,
+      opacity: 1,
+      duration: 3,
+      ease: 'power1.out',
+    },
+    'preload'
+  )
 
-          mainWrapper.scrollLeft -= screen.width;
+  tl.to(
+    nav,
+    {
+      opacity: 1,
+      duration: 1,
+      ease: 'sine.out',
+    },
+    'hero2'
+  )
 
-        }
-                
+  tl.to(
+    socials,
+    {
+      opacity: 1,
+      duration: 0.5,
+      ease: 'sine.out',
+    },
+    'hero2'
+  )
 
-      }
+  tl.to(
+    particles,
+    {
+      opacity: 1,
+      duration: 0.5,
+      ease: 'sine.out',
+    },
+    'hero2'
+  )
 
-    }
+  tl.to(copyr, {
+    opacity: 1,
+    duration: 0.5,
+    ease: 'sine.out',
+  })
 
-  });
+  tl.to(logo, {
+    opacity: 1,
+    duration: 0.5,
+    ease: 'sine.out',
+  })
+})
 
+// particles
+window.addEventListener('DOMContentLoaded', () => {
+  particlesJS('particles-js', {
+    particles: {
+      number: {
+        value: 70,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: '#ffffff',
+      },
+      shape: {
+        type: 'circle',
+        stroke: {
+          width: 0,
+          color: '#000000',
+        },
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+      },
+      size: {
+        value: 3,
+        random: true,
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: '#ffffff',
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: 'none',
+        random: false,
+        straight: false,
+        out_mode: 'out',
+      },
+    },
+    interactivity: {
+      detect_on: 'canvas',
+      events: {
+        onhover: {
+          enable: true,
+          mode: 'grab',
+        },
+        onclick: {
+          enable: true,
+          mode: 'push',
+        },
+      },
+      modes: {
+        grab: {
+          distance: 140,
+          line_linked: {
+            opacity: 1,
+          },
+        },
+        push: {
+          particles_nb: 4,
+        },
+      },
+    },
+    retina_detect: true,
+  })
+})
+
+// data displays
+document.addEventListener('DOMContentLoaded', () => {
+  const mainWrapper = document.querySelector('main')
+  const projectsWrapper = document.getElementById('projects')
 
   // skills
   const displayskills = async () => {
+    const url = location.origin
+    const response = await fetch(`${url}/src/data/skills.json`)
+    const skills = await response.json()
 
-    const url = location.origin;
-    const response = await fetch(`${url}/src/data/skills.json`);
-    const skills = await response.json();
+    const frontSkillsListWrapper = document.getElementById(
+      'skills__front--list'
+    )
+    let frontSkillsItems = ``
 
-    const frontSkillsListWrapper = document.getElementById('skills__front--list');
-    let frontSkillsItems = ``;
+    skills.front.map((skill) => {
+      frontSkillsItems += `<li class="skill">${skill}</li>`
+    })
 
-    skills.front.map(skill => {
-      frontSkillsItems += `<li class="skill">${skill}</li>`;
-    });
+    frontSkillsListWrapper.innerHTML += frontSkillsItems
 
-    frontSkillsListWrapper.innerHTML += frontSkillsItems;
+    const backSkillsListWrapper = document.getElementById('skills__back--list')
+    let backSkillsItems = ``
 
-    const backSkillsListWrapper = document.getElementById('skills__back--list');
-    let backSkillsItems = ``;
+    skills.back.map((skill) => {
+      backSkillsItems += `<li class="skill">${skill}</li>`
+    })
 
-    skills.back.map(skill => {
-      backSkillsItems += `<li class="skill">${skill}</li>`;
-    });
+    backSkillsListWrapper.innerHTML += backSkillsItems
 
-    backSkillsListWrapper.innerHTML += backSkillsItems;
+    const moreSkillsListWrapper = document.getElementById('skills__more--list')
+    let moreSkillsItems = ``
 
-    const moreSkillsListWrapper = document.getElementById('skills__more--list');
-    let moreSkillsItems = ``;
+    skills.more.map((skill) => {
+      moreSkillsItems += `<li class="skill">${skill}</li>`
+    })
 
-    skills.more.map(skill => {
-      moreSkillsItems += `<li class="skill">${skill}</li>`;
-    });
+    moreSkillsListWrapper.innerHTML += moreSkillsItems
+  }
 
-    moreSkillsListWrapper.innerHTML += moreSkillsItems;
-
-  } 
-
-  displayskills();
-  
+  displayskills()
 
   // experiences
   const displayExperiences = async () => {
+    const url = location.origin
+    const response = await fetch(`${url}/src/data/experiences.json`)
+    const experiences = await response.json()
 
-    const url = location.origin;
-    const response = await fetch(`${url}/src/data/experiences.json`);
-    const experiences = await response.json();
+    const experiencesListWrapper = document.getElementById('experiences-list')
+    let experiencesItems = ``
 
-    const experiencesListWrapper = document.getElementById('experiences-list');
-    let experiencesItems = ``;
-
-    experiences.data.map(experience => {
-
-      const {date, society, description} = experience;
+    experiences.data.map((experience) => {
+      const { date, society, description } = experience
 
       experiencesItems += `
 
@@ -112,31 +251,25 @@ document.addEventListener('DOMContentLoaded', (e) => {
               <p class="experiences__item--description">${description}</p>
             </div>
       
-      `;
+      `
+    })
 
-    });
+    experiencesListWrapper.innerHTML += experiencesItems
+  }
 
-
-    experiencesListWrapper.innerHTML += experiencesItems;
-
-  } 
-
-  displayExperiences();
-
+  displayExperiences()
 
   // education
   const displayEducation = async () => {
+    const url = location.origin
+    const response = await fetch(`${url}/src/data/education.json`)
+    const education = await response.json()
 
-    const url = location.origin;
-    const response = await fetch(`${url}/src/data/education.json`);
-    const education = await response.json();
+    const educationListWrapper = document.getElementById('education-list')
+    let educationItems = ``
 
-    const educationListWrapper = document.getElementById('education-list');
-    let educationItems = ``;
-
-    education.data.map(education => {
-
-      const {date, title, description, mention} = education;
+    education.data.map((education) => {
+      const { date, title, description, mention } = education
 
       educationItems += `
 
@@ -151,76 +284,75 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
             </div>
       
-      `;
+      `
+    })
 
-    });
+    educationListWrapper.innerHTML += educationItems
+  }
 
-
-    educationListWrapper.innerHTML += educationItems;
-
-  } 
-
-  displayEducation();
-
+  displayEducation()
 
   // career tabs
-  document.querySelectorAll('.career-tabs__item').forEach(tabItem => {
-    
+  document.querySelectorAll('.career-tabs__item').forEach((tabItem) => {
     tabItem.addEventListener('click', (e) => {
+      let currentTabItem = e.target.getAttribute('data-career')
 
-      let currentTabItem = e.target.getAttribute('data-career');
-      
-      if(currentTabItem == 'experiences'){
-
-        document.getElementById('experiences-list').classList.remove('display-none');
-        document.getElementById('education-list').classList.add('display-none');
-        document.querySelector('.career-tabs__item[data-career="experiences"]').classList.add('active');
-        document.querySelector('.career-tabs__item[data-career="education"]').classList.remove('active');
-
-      } else if(currentTabItem == 'education') {
-
-        document.getElementById('education-list').classList.remove('display-none');
-        document.getElementById('experiences-list').classList.add('display-none');
-        document.querySelector('.career-tabs__item[data-career="experiences"]').classList.remove('active');
-        document.querySelector('.career-tabs__item[data-career="education"]').classList.add('active');
-
-
+      if (currentTabItem == 'experiences') {
+        document
+          .getElementById('experiences-list')
+          .classList.remove('display-none')
+        document.getElementById('education-list').classList.add('display-none')
+        document
+          .querySelector('.career-tabs__item[data-career="experiences"]')
+          .classList.add('active')
+        document
+          .querySelector('.career-tabs__item[data-career="education"]')
+          .classList.remove('active')
+      } else if (currentTabItem == 'education') {
+        document
+          .getElementById('education-list')
+          .classList.remove('display-none')
+        document
+          .getElementById('experiences-list')
+          .classList.add('display-none')
+        document
+          .querySelector('.career-tabs__item[data-career="experiences"]')
+          .classList.remove('active')
+        document
+          .querySelector('.career-tabs__item[data-career="education"]')
+          .classList.add('active')
       }
-
-    });
-    
+    })
   })
-
 
   // projects
   const displayProjects = async (tech = 'all') => {
-
-    const url = location.origin;
-    const response = await fetch(`${url}/src/data/projects.json`);
-    const projects = await response.json();
-    const projectsSelected = [];
-    projects.data.map(project => {
-      if(tech !== 'all'){
-
-        if(project.technologies.tags.includes(tech)){
-            projectsSelected.push(project);
+    const url = location.origin
+    const response = await fetch(`${url}/src/data/projects.json`)
+    const projects = await response.json()
+    const projectsSelected = []
+    projects.data.map((project) => {
+      if (tech !== 'all') {
+        if (project.technologies.tags.includes(tech)) {
+          projectsSelected.push(project)
         }
-    
       } else {
-        projectsSelected.push(project);
+        projectsSelected.push(project)
       }
+    })
 
-    });
-        
-    const projectsListWrapper = document.querySelector('.projects__flux');
-    let projectsItems = ``;
+    const projectsListWrapper = document.querySelector('.projects__flux')
+    let projectsItems = ``
 
-    projectsSelected.map(project => {
+    projectsSelected.map((project) => {
+      const { name, description, thumbnail, technologies } = project
 
-      const {name, description, thumbnail, technologies} = project;
-
-      const github = project.github ? `<a href="${project.github}" class="github secondary-cta" title="View project files" target="_blank">Git</a>` : ``;
-      const link = project.link ? `<a href="${project.link}" class="live primary-cta" title="View live" target="_blank">Live</a>` : ``;
+      const github = project.github
+        ? `<a href="${project.github}" class="github secondary-cta" title="View project files" target="_blank">Git</a>`
+        : ``
+      const link = project.link
+        ? `<a href="${project.link}" class="live primary-cta" title="View live" target="_blank">Live</a>`
+        : ``
 
       projectsItems += `
 
@@ -247,157 +379,70 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
             </div>
       
-      `;
+      `
+    })
 
-    });
+    projectsListWrapper.innerHTML = projectsItems
+  }
 
-
-    projectsListWrapper.innerHTML = projectsItems;
-
-  } 
-  
-  displayProjects();
-
+  displayProjects()
 
   // projects filters
   const displayFilters = async () => {
+    const url = location.origin
+    const response = await fetch(`${url}/src/data/projects.json`)
+    const projects = await response.json()
 
-    const url = location.origin;
-    const response = await fetch(`${url}/src/data/projects.json`);
-    const projects = await response.json();
+    const projectsFiltersListWrapper = document.querySelector(
+      '.projects__filters--list'
+    )
+    let projectsFiltersItems = ``
 
-    const projectsFiltersListWrapper = document.querySelector('.projects__filters--list');
-    let projectsFiltersItems = ``;
+    const filtersValues = []
 
-    const filtersValues = [];
+    projects.data.map((project) => {
+      project.technologies.tags.map((technology) => {
+        filtersValues.push(technology)
+      })
+    })
 
-    projects.data.map(project => {
-      
-      project.technologies.tags.map(technology => {
-        filtersValues.push(technology);
-      });
+    const filters = Array.from(new Set(filtersValues)).sort()
 
-    });
+    projectsFiltersItems += `<li class="filter active" data-filter="all">all</li>`
 
-    const filters = Array.from(new Set(filtersValues)).sort();
-    
-    projectsFiltersItems += `<li class="filter active" data-filter="all">all</li>`;
-
-    filters.map(filter => {
-      if(filter !== 'prestashop'){
-        projectsFiltersItems += `<li class="filter" data-filter="${filter}">${filter}</li>`;
+    filters.map((filter) => {
+      if (filter !== 'prestashop') {
+        projectsFiltersItems += `<li class="filter" data-filter="${filter}">${filter}</li>`
       }
     })
 
-    projectsFiltersListWrapper.innerHTML = projectsFiltersItems;
+    projectsFiltersListWrapper.innerHTML = projectsFiltersItems
 
-    
+    const filterElements = document.querySelectorAll('.filter')
 
-
-    const filterElements = document.querySelectorAll('.filter');
-    
-    filterElements.forEach(filter => {
-
-
-
+    filterElements.forEach((filter) => {
       filter.addEventListener('click', (e) => {
+        let currFilter = e.target.getAttribute('data-filter')
 
-        let currFilter = e.target.getAttribute('data-filter');
+        e.target.classList.add('active')
+        let otherFilters = document.querySelectorAll(
+          `.filter:not([data-filter="${currFilter}"])`
+        )
 
-        e.target.classList.add('active');
-        let otherFilters = document.querySelectorAll(`.filter:not([data-filter="${currFilter}"])`);
-        
-        otherFilters.forEach(otherFilter => {
-            otherFilter.classList.remove('active');   
+        otherFilters.forEach((otherFilter) => {
+          otherFilter.classList.remove('active')
         })
-        
 
-        return displayProjects(currFilter);
-        
-
-      });
-          
+        return displayProjects(currFilter)
+      })
     })
-
   }
 
-  displayFilters();
-
+  displayFilters()
 
   // force open cv pdf in new window
   document.querySelector('.cv').addEventListener('click', (e) => {
-
-    e.preventDefault();
-    window.open(e.target.getAttribute('href'));
-
-  });
-
-
-  // hero section title animation
-  const letters = document.querySelectorAll('.letter');
-  let isAnimating = false;
-
-  function getRandomPosition(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  function applyRandomPositions() {
-    if (!isAnimating) {
-      isAnimating = true;
-
-      letters.forEach(letter => {
-        const randomX = getRandomPosition(-200, 200);
-        const randomY = getRandomPosition(-200, 200);
-        const randomRotate = getRandomPosition(-30, 30);
-        
-        letter.style.transform = `translate(${randomX}px, ${randomY}px) rotate(${randomRotate}deg)`;
-      });
-    }
-  }
-
-  function resetPositions() {
-    if (isAnimating){
-      letters.forEach(letter => {
-        letter.style.transform = `translate(0, 0) rotate(0deg)`;
-      });
-      isAnimating = false;
-    }
-  }
-
-  document.querySelector('.title__animated').addEventListener('mouseover', applyRandomPositions);
-  document.querySelector('.title__animated').addEventListener('mouseout', resetPositions);
-
-
-
-  // hero backgrounds
-  function getRandomBorderRadius() {
-    const percentage1 = Math.random() * 100;
-    const percentage2 = Math.random() * 100;
-    const percentage3 = Math.random() * 100;
-    const percentage4 = Math.random() * 100;
-    return `${percentage1}% ${percentage2}% ${percentage3}% ${percentage4}% / ${percentage1}% ${percentage2}% ${percentage3}% ${percentage4}%`;
-  }
-  
-  function updateBorderRadius() {
-    const radiusBefore = getRandomBorderRadius();
-    const radiusAfter = getRandomBorderRadius();
-  
-    const style = document.createElement('style');
-    style.textContent = `
-      .main__hero::before {
-        border-radius: ${radiusBefore};
-      }
-      .main__hero::after {
-        border-radius: ${radiusAfter};
-      }
-    `;
-    
-    document.head.appendChild(style);
-  }
-  
-  setInterval(updateBorderRadius, 3000);
-  
- 
-  
-
-});
+    e.preventDefault()
+    window.open(e.target.getAttribute('href'))
+  })
+})
