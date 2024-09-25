3c27d822c26fa927b47e7ec93765bc0b474327cb
Florence Bastaraud <fbastaraud@yahoo.fr>
2024-09-25 11:37:03 +0200

    projects section

--- src/js/main.js
+++ src/js/main.js
@@ -257,9 +257,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // data displays
 document.addEventListener('DOMContentLoaded', () => {
-  const mainWrapper = document.querySelector('main')
-  const projectsWrapper = document.getElementById('projects')
-
   // skills
   const displayskills = async () => {
     const url = location.origin
@@ -297,103 +294,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   displayskills()
-
-  // experiences
-  const displayExperiences = async () => {
-    const url = location.origin
-    const response = await fetch(`${url}/src/data/experiences.json`)
-    const experiences = await response.json()
-
-    const experiencesListWrapper = document.getElementById('experiences-list')
-    let experiencesItems = ``
-
-    experiences.data.map((experience) => {
-      const { date, society, description } = experience
-
-      experiencesItems += `
-
-            <div class="experiences__item">
-              <h3 class="experiences__item--society">
-                <i class="fas fa-network-wired icon"></i>
-                <span>${society}</span>
-              </h3>
-              <div class="experiences__item--date">${date}</div>
-              <p class="experiences__item--description">${description}</p>
-            </div>
-      
-      `
-    })
-
-    experiencesListWrapper.innerHTML += experiencesItems
-  }
-
-  // displayExperiences()
-
-  // education
-  const displayEducation = async () => {
-    const url = location.origin
-    const response = await fetch(`${url}/src/data/education.json`)
-    const education = await response.json()
-
-    const educationListWrapper = document.getElementById('education-list')
-    let educationItems = ``
-
-    education.data.map((education) => {
-      const { date, title, description, mention } = education
-
-      educationItems += `
-
-            <div class="education__item">
-              <h3 class="education__item--title">
-                <i class="fas fa-network-wired icon"></i>
-                <span>${title}</span>
-              </h3>
-              <div class="education__item--date">${date}</div>
-              <p class="education__item--description">${description}</p>
-              <span class="education__item--mention">${mention}</span>
-
-            </div>
-      
-      `
-    })
-
-    educationListWrapper.innerHTML += educationItems
-  }
-
-  // displayEducation()
-
-  // career tabs
-  document.querySelectorAll('.career-tabs__item').forEach((tabItem) => {
-    tabItem.addEventListener('click', (e) => {
-      let currentTabItem = e.target.getAttribute('data-career')
-
-      if (currentTabItem == 'experiences') {
-        document
-          .getElementById('experiences-list')
-          .classList.remove('display-none')
-        document.getElementById('education-list').classList.add('display-none')
-        document
-          .querySelector('.career-tabs__item[data-career="experiences"]')
-          .classList.add('active')
-        document
-          .querySelector('.career-tabs__item[data-career="education"]')
-          .classList.remove('active')
-      } else if (currentTabItem == 'education') {
-        document
-          .getElementById('education-list')
-          .classList.remove('display-none')
-        document
-          .getElementById('experiences-list')
-          .classList.add('display-none')
-        document
-          .querySelector('.career-tabs__item[data-career="experiences"]')
-          .classList.remove('active')
-        document
-          .querySelector('.career-tabs__item[data-career="education"]')
-          .classList.add('active')
-      }
-    })
-  })
 
   // projects
   const displayProjects = async (tech = 'all') => {
