65dadbfc10d313247c47802636008aa4ad5cd244
Florence Bastaraud <fbastaraud@yahoo.fr>
2024-09-25 14:03:20 +0200

    added back to top btn and func

--- src/js/main.js
+++ src/js/main.js
@@ -465,3 +465,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
   translatePage('en')
 })
+
+// back to top
+document.addEventListener('DOMContentLoaded', () => {
+  const backToTopButton = document.getElementById('back-to-top')
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+      backToTopButton.style.display = 'block'
+    } else {
+      backToTopButton.style.display = 'none'
+    }
+  })
+
+  backToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  })
+})
